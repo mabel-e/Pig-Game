748c3e46ddbb6795621cf40fca4ce7a2fcd3f124
mabel <mabel.egbirika.319@k12.friscosid.org>
2019-10-01 11:13:02 -0500

    basic lay out of pig game (10/1)

---
+++ Pig Game/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Pig Game
+//
+//  Created by Egbirika, Mabel C on 9/27/19.
+//  Copyright © 2019 Egbirika, Mabel C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var player1Label: UILabel!
+    @IBOutlet var player2Label: UILabel!
+    @IBOutlet var endOutlet: UIButton!
+    @IBAction func endButton(_ sender: Any) {
+        var x = 0
+    }
+    @IBAction func rolledButton(_ sender: Any) {
+    }
+    @IBOutlet var rollOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
